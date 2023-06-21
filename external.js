239b1fde8e94f6181afd1efc74d764b19c131238
AnuNari1997 <anupriyarathod1997@gmail.com>
2023-06-21 18:11:47 +0530

    first commit

---
+++ external.js
@@ -0,0 +1,26 @@
+function Shoes()
+        { 
+        document.getElementById('anupriya').src = "./Assets/shoes_Images/shoes.jpg";
+        document.getElementById('anupriya2').src = "./Assets/shoes_Images/women shoes.jpg";
+        document.getElementById('anupriya3').src = "./Assets/shoes_Images/wshoes.jpg";
+        }
+
+function Dresses()
+        { 
+        document.getElementById('anupriya').src = "./Assets/Dresses_Images/Anarkali.jpg";
+        document.getElementById('anupriya2').src = "./Assets/Dresses_Images/dress.jpg";
+        document.getElementById('anupriya3').src = "./Assets/Dresses_Images/frock.jpg";
+        }
+function HomeAccesses()
+        {
+            document.getElementById('anupriya').src = "./Assets/Home_Accesses/home machines.jpg";
+            document.getElementById('anupriya2').src = "./Assets/Home_Accesses/homeaccesceries.jpg";
+            document.getElementById('anupriya3').src = "./Assets/Home_Accesses/kitchenacceseries.jpg";
+        }
+function Babyproduts()
+        {
+            document.getElementById('anupriya').src = "./Assets/Bbaby_Produts/baby producets.jpg";
+            document.getElementById('anupriya2').src = "./Assets/Bbaby_Produts/baby toys.jpg";
+            document.getElementById('anupriya3').src = "./Assets/Bbaby_Produts/bbdyprds.jpg";
+        }
+        
